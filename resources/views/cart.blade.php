e3b18a6ce4312bf0818a3a621a906b2c3cd63e3a
MulkiAbrar <alkafiaboy20@gmail.com>
2025-06-10 06:31:42 +0700

    fix all

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -348,7 +348,6 @@ function updateQty(id, change) {
 
     <!-- Template Javascript -->
     <script src="{{ ("js/main.js") }}"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 
