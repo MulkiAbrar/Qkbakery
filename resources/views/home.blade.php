eb36d75710c12b915baa75b303fa53640331b798
MulkiAbrar <alkafiaboy20@gmail.com>
2025-06-09 20:46:35 +0700

    fix all

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -124,7 +124,7 @@
     <div class="container-fluid p-0 pb-5 wow fadeIn" data-wow-delay="0.1s">
         <div class="owl-carousel header-carousel position-relative">
             <div class="owl-carousel-item position-relative">
-                <img class="img-fluid" src="img/bg_Qkbakery.jpeg" alt="">
+                <img class="img-fluid" src="img/bg_Qkbakery.jpg" alt="">
                 <div class="owl-carousel-inner">
                     <div class="container">
                         <div class="row justify-content-start">
@@ -138,7 +138,7 @@
                 </div>
             </div>
             <div class="owl-carousel-item position-relative">
-                <img class="img-fluid" src="img/carousel-2.jpg" alt="">
+                <img class="img-fluid" src="img/bg-Qkbakery2.jpg" alt="">
                 <div class="owl-carousel-inner">
                     <div class="container">
                         <div class="row justify-content-start">
