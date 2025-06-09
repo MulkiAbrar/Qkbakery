2b43c4492c8d6ecaa8c6e6c0c0aab00671cee78c
MulkiAbrar <alkafiaboy20@gmail.com>
2025-06-10 01:04:24 +0700

    fix all

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -76,9 +76,12 @@
     </a>
     <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
         <span class="navbar-toggler-icon"></span>
+        
     </button>
-    <div class="collapse navbar-collapse" id="navbarCollapse">
-        <ul class="navbar-nav ms-auto p-4 p-lg-0 text-center w-100">
+   <div class="collapse navbar-collapse" id="navbarCollapse">
+    <div class="d-flex flex-column flex-lg-row align-items-center w-100">
+        <!-- Menu navigasi -->
+        <ul class="navbar-nav me-lg-auto p-4 p-lg-0 text-center">
             <li class="nav-item">
                 <a href="{{ url('home') }}" class="nav-link">{{ __('messages.home') }}</a>
             </li>
@@ -86,7 +89,7 @@
                 <a href="{{ url('about') }}" class="nav-link">{{ __('messages.about') }}</a>
             </li>
             <li class="nav-item">
-                <a href="{{ url("product") }}" class="nav-link">{{ __('messages.product') }}</a>
+                <a href="{{ url('product') }}" class="nav-link">{{ __('messages.product') }}</a>
             </li>
             <li class="nav-item">
                 <a href="{{ url('kontak') }}" class="nav-link">{{ __('messages.contact') }}</a>
@@ -94,15 +97,17 @@
             <li class="nav-item">
                 <a href="{{ url('/admin/login') }}" class="nav-link">{{ __('messages.admin') }}</a>
             </li>
-            <li class="nav-item" >
-                <a href="{{ url('cart') }}" class="nav-link">
-                    <img src="{{ asset('img/icons8-cart-50.png') }}" alt="Cart" width="28" class="hover-image" style="padding-top: 1px;">
-                </a>
-            </li>
-
         </ul>
+
+        <!-- Keranjang belanja -->
+        <div class="text-center mt-3 mt-lg-0">
+            <a href="{{ url('cart') }}" class="nav-link">
+                <img src="{{ asset('img/icons8-cart-50.png') }}" alt="Cart" width="28" class="hover-image">
+            </a>
         </div>
     </div>
+</div>
+
 
 
     <!-- Hanya tampil di desktop -->
