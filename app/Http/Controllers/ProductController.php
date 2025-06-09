114164353616dee4d94fc4e6ea23175adc630adf
MulkiAbrar <alkafiaboy20@gmail.com>
2025-06-09 15:12:02 +0700

    fix error

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -55,7 +55,7 @@ class ProductController extends Controller
         'deskripsi' => $request->deskripsi
     ]);
 
-    return redirect()->route('product.editAdmin')->with('success', 'Produk berhasil ditambahkan');
+    return redirect()->route('product.index')->with('success', 'Produk berhasil ditambahkan');
 }
 
 
