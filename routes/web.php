50738e1fc26c7a0d09126c9ab62ba176c690ff22
MulkiAbrar <alkafiaboy20@gmail.com>
2025-06-09 16:29:58 +0700

    fix error

--- routes/web.php
+++ routes/web.php
@@ -30,10 +30,10 @@ Route::get('/adminproduct', [ProductController::class , 'admin'])->name('product
 Route::get('product/{id}/edit', [ProductController::class, 'edit'])->name('product.edit');
 Route::put('product/{id}', [ProductController::class, 'update'])->name('product.update');
 
-Route::get('/admin/products', [ProductController::class, 'admin'])->name('product.editAdmin');
+Route::get('/admin/products', [ProductController::class, 'admin'])->name('products.editAdmin');
 Route::get('/admin/products/{id}/edit', [ProductController::class, 'edit'])->name('products.edit');
-Route::post('/admin/products/{id}/update', [ProductController::class, 'update'])->name('products.update');
-Route::post('/admin/products/{id}/destroy', [ProductController::class, 'destroy'])->name('products.destroy');
+Route::put('/admin/products/{id}/update', [ProductController::class, 'update'])->name('products.update');
+Route::delete('/admin/products/{id}/destroy', [ProductController::class, 'destroy'])->name('products.destroy');
 
 // Admin khusus product
 Route::get('/admin/products', [ProductController::class, 'admin'])->name('products.admin');
