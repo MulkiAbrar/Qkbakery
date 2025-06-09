06faf30d3bc6e3b54efe304ca4bffab908992b77
MulkiAbrar <alkafiaboy20@gmail.com>
2025-06-09 12:01:54 +0700

    add Order.php

--- app/Models/order.php
+++ app/Models/order.php
@@ -4,15 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class order extends Model
+class Order extends Model
 {
     protected $fillable = ['id','user_name', 'user_address', 'user_phone', 'product_id', 'quantity', 'total_price', 'status','payment_proof'];
 
-    public function product()
-    {
-        return $this->belongsTo(Product::class);
-    }
-    protected $casts = [
-        'user_data' => 'array',
-    ];
+        public function product()
+        {
+            return $this->belongsTo(Product::class);
+        }
+        protected $casts = [
+            'user_data' => 'array',
+        ];
 }
