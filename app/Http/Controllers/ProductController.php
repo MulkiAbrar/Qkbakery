a4e2994cb9049af642ee4d38188750043215f43b
MulkiAbrar <alkafiaboy20@gmail.com>
2025-06-09 16:05:52 +0700

    fix error

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -28,40 +28,35 @@ class ProductController extends Controller
 
     public function store(Request $request)
 {
-    // Validasi input
     $request->validate([
-        'nama' => 'required|string|max:255',
+        'nama' => 'required',
         'gambar' => 'nullable|image|mimes:png,jpg,jpeg|max:2048',
         'harga' => 'required|numeric',
-        'deskripsi' => 'nullable|string'
+        'deskripsi' => 'nullable'
     ]);
 
-    // Inisialisasi path gambar
     $imagePath = null;
 
-    // Proses upload gambar jika ada
     if ($request->hasFile('gambar')) {
         $gambar = $request->file('gambar');
-        $namaFile = time() . '_' . preg_replace('/\s+/', '_', strtolower($gambar->getClientOriginalName()));
+        $namaFile = time() . '_' . $gambar->getClientOriginalName();
 
         // Simpan ke public/img/produk
         $gambar->move(public_path('img/produk'), $namaFile);
 
-        // Path relatif ke folder public
+        // Simpan path relatif dari folder public
         $imagePath = 'img/produk/' . $namaFile;
     }
 
-    // Simpan data ke database
     Product::create([
         'nama' => $request->nama,
-        'gambar' => $imagePath,
+        'gambar' => $imagePath, // Simpan path relatif
         'harga' => $request->harga,
         'deskripsi' => $request->deskripsi
     ]);
 
     return redirect()->route('product.index')->with('success', 'Produk berhasil ditambahkan');
 }
-
 
 
 
