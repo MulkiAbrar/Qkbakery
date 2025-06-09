ecf7e931f41c477fb49c0e8e878919aa2b850f55
MulkiAbrar <alkafiaboy20@gmail.com>
2025-06-09 12:29:51 +0700

    fix error

---
+++ app/Models/Order.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    protected $fillable = ['id','user_name', 'user_address', 'user_phone', 'product_id', 'quantity', 'total_price', 'status','payment_proof'];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+    protected $casts = [
+        'user_data' => 'array',
+    ];
+}
