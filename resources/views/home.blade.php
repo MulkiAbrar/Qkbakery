fef9eea07cb457551fc9fc36d24d26a3b6b88944
MulkiAbrar <alkafiaboy20@gmail.com>
2025-06-10 06:27:06 +0700

    fix all

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,7 +19,7 @@
     <!-- Icon Font Stylesheet -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
-    
+
 
 
 
@@ -378,7 +378,6 @@
 
     <!-- Template Javascript -->
     <!-- Bootstrap JS (wajib untuk hamburger menu) -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <script src="{{ ("js/main.js") }}"></script>
 </body>
 
