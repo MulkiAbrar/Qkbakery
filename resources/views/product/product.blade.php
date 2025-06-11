8e0fd0daf5c8f16df614f4b21d3cc3e5e3b97075
MulkiAbrar <alkafiaboy20@gmail.com>
2025-06-12 02:02:37 +0700

    fix all

--- resources/views/product/product.blade.php
+++ resources/views/product/product.blade.php
@@ -126,7 +126,7 @@
         <!-- Cart in mobile only -->
         <li class="nav-item d-block d-lg-none mt-2">
             <a href="{{ url('cart') }}" class="nav-link">
-                <img src="{{ asset('img/icons8-cart-50.png') }}" alt="Cart" width="28">
+                <img src="{{ asset('img/icons8-cart-50.png') }}" class="hover-image" alt="Cart" width="28">
             </a>
         </li>
     </ul>
@@ -134,7 +134,7 @@
     <!-- Cart in desktop only -->
     <div class="d-none d-lg-block">
         <a href="{{ url('cart') }}" class="nav-link">
-            <img src="{{ asset('img/icons8-cart-50.png') }}" alt="Cart" width="28">
+            <img src="{{ asset('img/icons8-cart-50.png') }}" class="hover-image" alt="Cart" width="28">
         </a>
     </div>
 </div>
