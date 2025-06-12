9f73b14dd3acabbed56de9fd803796718e35becf
MulkiAbrar <alkafiaboy20@gmail.com>
2025-06-12 20:03:24 +0700

    fix all

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -2,19 +2,12 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    use HasFactory;
-
-    protected $fillable = ['user_id', 'name', 'email', 'subject', 'message'];
-
-    public function user()
-    {
-        return $this->belongsTo(User::class);
-    }
+    protected $fillable = ['name', 'email', 'subject', 'message'];
 }
+
 
 
--- database/migrations/2025_06_12_082824_create_reviews_table.php
+++ database/migrations/2025_06_12_082824_create_reviews_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
     {
         Schema::create('reviews', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('user_id');
             $table->string('name');
             $table->string('email');
+            $table->string('subject');
             $table->text('message');
             $table->timestamps();
         });
