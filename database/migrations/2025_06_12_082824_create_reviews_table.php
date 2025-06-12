9767a7092ec66ec6a7e83028ce96c58f21f6cda3
MulkiAbrar <alkafiaboy20@gmail.com>
2025-06-12 19:45:21 +0700

    fix all

--- database/migrations/2025_06_12_082824_create_reviews_table.php
+++ database/migrations/2025_06_12_082824_create_reviews_table.php
@@ -9,24 +9,24 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
-        Schema::create('contacts', function (Blueprint $table) {
+        Schema::create('reviews', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
             $table->string('name');
             $table->string('email');
-            $table->string('subject');
             $table->text('message');
             $table->timestamps();
         });
-
     }
+
 
     /**
      * Reverse the migrations.
      */
     public function down(): void
     {
-        Schema::dropIfExists('contacts');
+        Schema::dropIfExists('reviews');
     }
 };
