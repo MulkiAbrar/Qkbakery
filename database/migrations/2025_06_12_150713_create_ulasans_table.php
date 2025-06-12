84711b6857e1713d8f42b19e506e623fa248d0e7
MulkiAbrar <alkafiaboy20@gmail.com>
2025-06-12 22:22:48 +0700

    fix all

--- database/migrations/2025_06_12_150713_create_ulasans_table.php
+++ database/migrations/2025_06_12_150713_create_ulasans_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUlasanTable extends Migration
+class Ulasan extends Migration
 {
     public function up(): void
     {
@@ -13,7 +13,7 @@ class CreateUlasanTable extends Migration
             $table->string('name');
             $table->string('email');
             $table->string('subject');
-            $table->text('message');  
+            $table->text('message');
             $table->timestamps();
         });
     }
